2626d0cee0f40b1448528ecc19367f2b8a15f502
Jaden <jenzbr@gmail.com>
2023-04-11 18:04:05 -0500

    Added relative time, postView component, main page styling, changed post route

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,10 +7,10 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-  <ClerkProvider {...pageProps}>
-     <Component {...pageProps} />;
-  </ClerkProvider>
-  )
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
